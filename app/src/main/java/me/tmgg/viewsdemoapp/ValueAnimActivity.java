78da923217d7fea797d378c77bef4ef0d1e0e214
sunwei <tianmu19@gmail.com>
2019-03-11 13:58:13 +0800

    卫星菜单修复一些bug

--- app/src/main/java/me/tmgg/viewsdemoapp/ValueAnimActivity.java
+++ app/src/main/java/me/tmgg/viewsdemoapp/ValueAnimActivity.java
@@ -48,23 +48,26 @@ public class ValueAnimActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (isOpenMenu) {
-                    doAnimateClose(mItemButton1, 0, 5, 300);
-                    doAnimateClose(mItemButton2, 1, 5, 300);
-                    doAnimateClose(mItemButton3, 2, 5, 300);
-                    doAnimateClose(mItemButton4, 3, 5, 300);
-                    doAnimateClose(mItemButton5, 4, 5, 300);
+                    doAnimateClose(mItemButton1, 0, 5, RADIUS);
+                    doAnimateClose(mItemButton2, 1, 5, RADIUS);
+                    doAnimateClose(mItemButton3, 2, 5, RADIUS);
+                    doAnimateClose(mItemButton4, 3, 5, RADIUS);
+                    doAnimateClose(mItemButton5, 4, 5, RADIUS);
                 } else {
-                    doAnimateOpen(mItemButton1, 0, 5, 300);
-                    doAnimateOpen(mItemButton2, 1, 5, 300);
-                    doAnimateOpen(mItemButton3, 2, 5, 300);
-                    doAnimateOpen(mItemButton4, 3, 5, 300);
-                    doAnimateOpen(mItemButton5, 4, 5, 300);
+                    doAnimateOpen(mItemButton1, 0, 5, RADIUS);
+                    doAnimateOpen(mItemButton2, 1, 5, RADIUS);
+                    doAnimateOpen(mItemButton3, 2, 5, RADIUS);
+                    doAnimateOpen(mItemButton4, 3, 5, RADIUS);
+                    doAnimateOpen(mItemButton5, 4, 5, RADIUS);
                 }
                 isOpenMenu = !isOpenMenu;
             }
         });
 
     }
+
+    private static final long DURATION = 350L;
+    private static final long RADIUS = 550L;
 
     private void doAnimateOpen(Button view, int index, int total, long radius) {
         if (view.getVisibility() != View.VISIBLE) {
@@ -76,10 +79,10 @@ public class ValueAnimActivity extends AppCompatActivity {
         AnimatorSet set = new AnimatorSet(); //包含平移、缩放和透明度动画
         set.playTogether(ObjectAnimator.ofFloat(view, "translationX", 0, translationX), ObjectAnimator.ofFloat(view, "translationY", 0, translationY), ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f), ObjectAnimator.ofFloat(view, "scaleY", 0f, 1f), ObjectAnimator.ofFloat(view, "alpha", 0f, 1));
         //动画周期为500ms
-        set.setDuration(1 * 500).start();
+        set.setDuration(DURATION).start();
     }
 
-    private void doAnimateClose(final View view, int index, int total, int radius) {
+    private void doAnimateClose(final View view, int index, int total, long radius) {
         if (view.getVisibility() != View.VISIBLE) {
             view.setVisibility(View.VISIBLE);
         }
@@ -87,8 +90,8 @@ public class ValueAnimActivity extends AppCompatActivity {
         int translationX = -(int) (radius * Math.sin(degree));
         int translationY = -(int) (radius * Math.cos(degree));
         AnimatorSet set = new AnimatorSet(); //包含平移、缩放和透明度动画
-        set.playTogether(ObjectAnimator.ofFloat(view, "translationX", translationX, 0), ObjectAnimator.ofFloat(view, "translationY", translationY, 0), ObjectAnimator.ofFloat(view, "scaleX", 1f, 0f), ObjectAnimator.ofFloat(view, "scaleY", 1f, 0.1f), ObjectAnimator.ofFloat(view, "alpha", 1f, 0f));
-        set.setDuration(1 * 500).start();
+        set.playTogether(ObjectAnimator.ofFloat(view, "translationX", translationX, 0), ObjectAnimator.ofFloat(view, "translationY", translationY, 0), ObjectAnimator.ofFloat(view, "scaleX", 1f, 0.1f), ObjectAnimator.ofFloat(view, "scaleY", 1f, 0.1f), ObjectAnimator.ofFloat(view, "alpha", 1f, 0f));
+        set.setDuration(DURATION).start();
     }
         //扫描动画
         private int index = 0;
